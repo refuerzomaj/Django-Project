e36b586713d3a7fbe8700f278e44256fd8ae767f
unknown <refuerzomaj@gmail.com>
2023-10-15 12:52:35 +0800

    my second commit

--- my_first_app/views.py
+++ my_first_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse, redirect
 from .models import Member
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from . forms import SignUpForm
 # Create your views here.
 def home(request):
     #Check to see if logging in
@@ -34,4 +34,18 @@ def logout_user(request):
     return redirect('home')
     
 def register_user(request):
-    return render(request,'register.html')
+    if request.method =="POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request,user)
+            messages.success(request,"You have successfully register!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request,'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
